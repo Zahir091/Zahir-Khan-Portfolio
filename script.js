b16e7c0cdcd530670cc53457d5251ef94eb5bdf2
ZAHIR <121049450+Zahir091@users.noreply.github.com>
2023-01-11 02:36:14 +0000

    - Added links to each project - Made navbar and dropdown menu clickable - Used jQuery to show/hide the different pages - Added Home Button on navbar

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-$(".projectPage").hide();
+$(".htmlProjectPage, .pythonProjectPage, .otherProjectsPage, .aboutMePage, .contactsPage").hide();
 $(".homePage").show();
 
-$("#projectsButton").click(function() {
-  $(".projectPage").show();
-  $(".homePage").hide();
+$("#html").click(function() {
+  $(".htmlProjectPage").show();
+  $(".homePage, .pythonProjectPage, .otherProjectsPage, .aboutMePage, .contactsPage").hide();
 });
 
-$(".navLeft").click(function() {
-  $(".projectPage").hide();
+$("#python").click(function() {
+  $(".pythonProjectPage").show();
+  $(".homePage, .otherProjectsPage, .htmlProjectPage, .aboutMePage, .contactsPage").hide();
+});
+
+$("#other").click(function() {
+  $(".otherProjectsPage").show();
+  $(".homePage, .pythonProjectPage, .htmlProjectPage, .aboutMePage, .contactsPage").hide();
+});
+
+$("#homeButton").click(function() {
+  $(".htmlProjectPage, .pythonProjectPage, .otherProjectsPage, .aboutMePage, .contactsPage").hide();
   $(".homePage").show();
+});
+
+$("#aboutMeButton").click(function() {
+  $(".aboutMePage").show();
+  $(".homePage, .otherProjectsPage, .htmlProjectPage, .pythonProjectPage, .contactsPage").hide();
+});
+
+$("#contactsButton").click(function() {
+  $(".contactsPage").show();
+  $(".homePage, .otherProjectsPage, .htmlProjectPage, .aboutMePage, .pythonProjectPage").hide();
 });
